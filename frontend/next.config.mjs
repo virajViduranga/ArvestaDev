12c1459dca53217467ce1c8c5d582bf6fd87fecd
Viraj Viduranga <163404822+virajViduranga@users.noreply.github.com>
2026-09-26 19:23:43 +0530

    update next.config.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+   output: 'export',
+  images: {
+    unoptimized: true, // Required for static export
+  },
   reactCompiler: true,
 };
 
 export default nextConfig;
+
